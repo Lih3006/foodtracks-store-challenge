5234b745ee814324eff2bd2083624cc7e19f0aa7
alineb3006 <alineb3006@hotmail.com>
2023-11-23 17:13:29 -0300

    set permissions

--- branches/permissions.py
+++ branches/permissions.py
@@ -1,25 +1,14 @@
 from rest_framework import permissions
 from rest_framework.views import Request, View
 from .models import Branch
-from companies.models import Company
-
-
-from rest_framework import permissions
-from rest_framework.views import Request, View
-from .models import Branch
 
 
 class IsLinkedToBranch(permissions.BasePermission):
     def has_permission(self, req: Request, view: View):
-        # Permitir apenas superusuários, gerentes regionais e gerentes de site para criar
         if req.method == "POST":
-            return req.user.is_superuser or req.user.role in [
-                "regional_manager",
-                "site_manager",
-            ]
+            return req.user.is_superuser
         if req.method == "DELETE":
             return req.user.is_superuser
-        # Permitir leitura (GET) para todos os usuários
         return True
 
     def has_object_permission(self, req: Request, view: View, obj: Branch):
