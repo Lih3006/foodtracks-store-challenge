2cdd05e044ed9df07a26bc9a6218cb88db8db50c
alineb3006 <alineb3006@hotmail.com>
2023-11-29 09:52:53 +0100

    fix spaces and imports in differents files

--- operating_hours/views.py
+++ operating_hours/views.py
@@ -1,27 +1,19 @@
-from rest_framework.views import APIView, Request, Response, status
 from .models import Operating
 from .serializers import OperatingSerializer
-
-# from django.shortcuts import get_object_or_404
 from rest_framework_simplejwt.authentication import JWTAuthentication
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from branches.permissions import IsLinkedToBranch
 
 
 class ListCreateOperatingView(ListCreateAPIView):
     authentication_classes = [JWTAuthentication]
-    permission_classes = [IsAuthenticated, IsLinkedToBranch]
+    permission_classes = [IsLinkedToBranch]
     queryset = Operating.objects.all()
     serializer_class = OperatingSerializer
-
-    """     def perform_create(self, serializer):
-        print("Aqui", self)
-        serializer.save() """
 
 
 class RetrieveUpdateDeleteOperatingView(RetrieveUpdateDestroyAPIView):
     authentication_classes = [JWTAuthentication]
-    permission_classes = [IsAuthenticated, IsLinkedToBranch]
+    permission_classes = [IsLinkedToBranch]
     queryset = Operating.objects.all()
     serializer_class = OperatingSerializer
