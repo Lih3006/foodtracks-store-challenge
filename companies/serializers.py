9ed25a99933a9d6d9511b6ff9b051b4829187283
alineb3006 <alineb3006@hotmail.com>
2023-11-29 06:52:26 +0100

    change  ListCreateApiView

--- companies/serializers.py
+++ companies/serializers.py
@@ -1,16 +1,17 @@
 from rest_framework import serializers
 from .models import Company
+from django.db import IntegrityError
 
 
 class CompanySerializer(serializers.ModelSerializer):
-    # owner = AccountSerializer(read_only=True)
-
     class Meta:
         model = Company
         fields = ["id", "company_name"]
         read_only_fiels = ["id"]
 
     def create(self, validated_data):
-        return Company.objects.create(**validated_data)
-
-    # TODO vincular id do user
+        try:
+            return Company.objects.create(**validated_data)
+        except IntegrityError:
+            message = "You already have a company. Each user can have only one company."
+            raise serializers.ValidationError({"message": message})
